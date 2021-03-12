f95262e58383c2bdb46080276ae93c52704f0594
Robyn Taylor <rhtaylor0715@gmail.com>
2021-03-12 16:13:32 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -47,7 +47,7 @@ def draw?(board)
 end
 
 def over?(board)
-  if won?(board) && full?(board) && draw?(board)
+  if draw?(board) || won?(board) && full?(board) || won?(board) && !full?(board)
     return true
   else
     return false
